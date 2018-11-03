971eb25f226a8774a187981d91f207b837db75d6
andrewjshilliday <42033118+andrewjshilliday@users.noreply.github.com>
2018-11-03 14:59:43 +0000

    03/11/2018 changes

--- src/app/artists/artists.component.ts
+++ src/app/artists/artists.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { MusicService } from '../music.service';
-import { isAwaitExpression } from 'typescript';
 
 @Component({
   selector: 'app-artists',
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,3 +1,7 @@
 export class Utils {
 
+  static getArtistArtwork(url: string) {
+    
+  }
+
 }
