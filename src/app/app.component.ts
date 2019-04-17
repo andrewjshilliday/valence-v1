397801df647979b21c51975d830e5952f1e6f8d7
andrewjshilliday <42033118+andrewjshilliday@users.noreply.github.com>
2019-04-17 22:13:36 +0100

    route transition animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from './animations/fade.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
 
 }
